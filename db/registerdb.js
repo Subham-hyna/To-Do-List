8b9767872587b0089fd27dbd31648254e6501a69
Subham <subhamdutta460@gmail.com>
2023-08-17 21:31:09 +0530

    db update

--- db/registerdb.js
+++ db/registerdb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 // mongoose.connect("mongodb://127.0.0.1:27017/registerDB")
-mongoose.connect(process.env.DB)
+mongoose.connect("mongodb+srv://subhamdutta:subham@cluster0.vrb602n.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{console.log("Database Connection Successful")})
 .catch((err) => console.log(err))
